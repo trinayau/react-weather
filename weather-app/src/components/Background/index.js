2962149ef314060cbcc3079c49ec10af453abeb1
Trina <92634994+trinayau@users.noreply.github.com>
2022-07-20 21:12:54 +0100

    added unsplash referral link

--- weather-app/src/components/Background/index.js
+++ weather-app/src/components/Background/index.js
@@ -10,25 +10,25 @@ const Background = ({city}) => {
     const [photoLocation, setPhotoLocation] = useState('');
 
 
-    // useEffect(() => {
-    //     async function searchApi(city) {
-    //         try{
-    //             const result = await axios.get(`https://api.unsplash.com/search/photos/?query=${city}&client_id=${process.env.REACT_APP_UNSPLASH_KEY}`);
-    //             const randomPhoto = result.data.results[Math.floor(Math.random() * result.data.results.length)];
-    //             setBackgroundUrl(randomPhoto.urls.regular);
-    //             setPhotographer(randomPhoto.user.name);
-    //             setPhotographerUrl(randomPhoto.user.links.html);
-    //             setPhotoUrl(randomPhoto.links.html);
-    //             //get photo location
-    //             const location = await axios.get(`https://api.unsplash.com/photos/${randomPhoto.id}?client_id=${process.env.REACT_APP_UNSPLASH_KEY}`)
-    //             setPhotoLocation(location.data.location.title)
+    useEffect(() => {
+        async function searchApi(city) {
+            try{
+                const result = await axios.get(`https://api.unsplash.com/search/photos/?query=${city}&client_id=${process.env.REACT_APP_UNSPLASH_KEY}`);
+                const randomPhoto = result.data.results[Math.floor(Math.random() * result.data.results.length)];
+                setBackgroundUrl(randomPhoto.urls.regular);
+                setPhotographer(randomPhoto.user.name);
+                setPhotographerUrl(randomPhoto.user.links.html.concat('?utm_source=trina_react_weather&utm_medium=referral'));
+                setPhotoUrl(randomPhoto.links.html.concat('?utm_source=trina_react_weather&utm_medium=referral'));
+                //get photo location
+                const location = await axios.get(`https://api.unsplash.com/photos/${randomPhoto.id}?client_id=${process.env.REACT_APP_UNSPLASH_KEY}`)
+                setPhotoLocation(location.data.location.title)
                 
-    //         }catch(err){
-    //             console.error(err)
-    //         }
-    //     }
-    //     searchApi(city);
-    // }, [city]);
+            }catch(err){
+                console.error(err)
+            }
+        }
+        searchApi(city);
+    }, [city]);
 
     let divStyle;
 
@@ -71,7 +71,7 @@ const Background = ({city}) => {
     </div>
     <div className='unsplash-description'>
         <p>{photoLocation}</p>
-        <p><a href={photoUrl ? photoUrl : 'https://unsplash.com/photos/u4uGoUBxmCE'} target="_blank" rel='noreferrer'>Photo</a> / <a href={photographerUrl ? photographerUrl : 'https://unsplash.com/@wildlittlethingsphoto'}>{photographer ? photographer : 'Helena Lopes'}</a> / <a href='https://unsplash.com/'>Unsplash</a></p>
+        <p><a href={photoUrl ? photoUrl : 'https://unsplash.com/photos/u4uGoUBxmCE?utm_source=trina_react_weather&utm_medium=referral'} target="_blank" rel='noreferrer'>Photo</a> / <a href={photographerUrl ? photographerUrl : 'https://unsplash.com/@wildlittlethingsphoto?utm_source=trina_react_weather&utm_medium=referral'} target="_blank" rel='noreferrer'>{photographer ? photographer : 'Helena Lopes'}</a> / <a href='https://unsplash.com/?utm_source=trina_react_weather&utm_medium=referral' target="_blank" rel='noreferrer'>Unsplash</a></p>
     </div>
     </> );
 }
